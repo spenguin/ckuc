6866f52a8cbc5dfdc61f2c60d158a592afd8ed7a
spenguin <inof@soaringpenguin.com>
2025-12-31 21:58:16 -0800

    Code menu structure and basic styling.

---
+++ template-parts/header-main-navigation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Header Main Navigation 
+ */
+?>
+<div class="header__main-navigation max-wrapper">
+    <div class="header__main-navigation--menu">
+        Menu
+    </div>
+    <div class="header__main-navigation--logo">
+        <img src="<?php echo CORE_TEMPLATE_URL; ?>/assets/furniture/ckulogo.png" alt="Cool Kids Upstairs Collective" />
+    </div>
+    <div class="header__main-navigation--search">
+        <input type="text" name="search" />
+    </div>
+    <div class="header__main-navigation--cart">
+        Cart
+    </div>    
+</div>
---
+++ template-parts/header-top-bar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Header Top Bar links
+ */
+?>
+<div class="header__top-bar max-wrapper">
+    <div class="header__top-bar--primary">
+        <a href="" class="header__top-bar--email">Email</a>
+        <a href="" class="header__top-bar--support">Support</a>
+    </div>
+    <a href="" class="header__top-bar--login">Login</a>
+</div>
