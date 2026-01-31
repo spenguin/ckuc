cc6f0548edda1457e4bd42bf620d59f2ab98c97f
spenguin <inof@soaringpenguin.com>
2026-01-30 23:35:35 -0800

    Correct Product input.

--- includes/custom-fields/Products.php
+++ includes/custom-fields/Products.php
@@ -21,7 +21,7 @@ function saveProductSubtitle()
 
     if( empty($post->ID) ) return;
 
-    $subtitle   = wp_kses( $_POST['subtitle'] );
+    $subtitle   = wp_kses( $_POST['subtitle'], 'strip' );
     update_post_meta($post->ID, 'subtitle', $subtitle);
 }
 
@@ -31,7 +31,7 @@ function productSpecifications()
 
     $pageCount  = get_post_meta($post->ID, 'pageCount', TRUE );
     $pageColour = get_post_meta($post->ID, 'pageColour', TRUE );
-    $dimensions = get_post_meta($post->ID, 'dimensions' );
+    $dimensions = unserialize(get_post_meta($post->ID, 'dimensions', TRUE )); 
     $dimensions = $dimensions ? $dimensions : [NULL,NULL];
     $dimensionsSystem   = get_post_meta($post->ID, 'dimensionsSystem', TRUE);
     $weight     = get_post_meta($post->ID, 'weight', TRUE );
@@ -40,7 +40,7 @@ function productSpecifications()
     ?>
 
     <label for="pageCount">Page Count:</label>
-    <input type="number" name="pageCount" value="<?php echo $pageCount; ?>" size="1"/><br><br>
+    <input type="number" name="pageCount" value="<?php echo $pageCount; ?>" size="3"/><br><br>
 
     <label for="pageColour">Page Colour:</label><br>
     <input type="radio" name="pageColour" value="0" <?php echo $pageColour == 0 ? 'checked="checked"' : ''; ?> >Black & White<br>    
@@ -48,8 +48,8 @@ function productSpecifications()
     <input type="radio" name="pageColour" value="2" <?php echo $pageColour == 2 ? 'checked="checked"' : ''; ?>>Black & White/Colour<br><br>   
 
     <label>Dimensions:</label><br>
-    <label for="dimensions">Width:</label><input type="text" name="dimensions[]" value="<?php echo $dimensions[0]; ?>" size="1" />
-    <label for="dimensions">Height:</label><input type="text" name="dimensions[]" value="<?php echo $dimensions[1]; ?>" size="1" />
+    <label for="dimensions">Width:</label><input type="text" name="dimensions[]" value="<?php echo $dimensions[0]; ?>" size="3" />
+    <label for="dimensions">Height:</label><input type="text" name="dimensions[]" value="<?php echo $dimensions[1]; ?>" size="3" />
     <input type="radio" name="dimensionsSystem" value=0 <?php echo $dimensionsSystem == 0 ? 'checked="checked"' : ''; ?> />in<input type="radio" name="dimensionsSystem" value=1 <?php echo $dimensionsSystem == 1 ? 'checked="checked"' : ''; ?> />cm<br><br>
 
 
@@ -69,25 +69,25 @@ function saveProductSpecifications()
 
     if( empty($post->ID) ) return;
 
-    $pageCount   = wp_kses( $_POST['pageCount'] );
+    $pageCount   = wp_kses( $_POST['pageCount'], 'strip' );
     update_post_meta($post->ID, 'pageCount', $pageCount);
 
-    $pageColour   = wp_kses( $_POST['pageColour'] );
+    $pageColour   = wp_kses( $_POST['pageColour'], 'strip' );
     update_post_meta($post->ID, 'pageColour', $pageColour); 
     
-    $dimensions   = wp_kses( $_POST['dimensions'] );
+    $dimensions   = serialize(array_map( 'esc_attr', $_POST['dimensions'] )); 
     update_post_meta($post->ID, 'dimensions', $dimensions);   
     
-    $dimensionsSystem   = wp_kses( $_POST['dimensionsSystem'] );
+    $dimensionsSystem   = wp_kses( $_POST['dimensionsSystem'], 'strip' );
     update_post_meta($post->ID, 'dimensionsSystem', $dimensionsSystem);  
     
-    $weight   = wp_kses( $_POST['weight'] );
+    $weight   = wp_kses( $_POST['weight'], 'strip' );
     update_post_meta($post->ID, 'weight', $weight); 
     
-    $weightSystem   = wp_kses( $_POST['weightSystem'] );
+    $weightSystem   = wp_kses( $_POST['weightSystem'], 'strip' );
     update_post_meta($post->ID, 'weightSystem', $weightSystem);    
 
-    $binding   = wp_kses( $_POST['binding'] );
+    $binding   = wp_kses( $_POST['binding'], 'strip' );
     update_post_meta($post->ID, 'binding', $binding);    
 }
 
@@ -111,7 +111,7 @@ function savePDFLink()
 
     if( empty($post->ID) ) return;
 
-    $PDFLink   = wp_kses( $_POST['PDFLink'] );
+    $PDFLink   = wp_kses( $_POST['PDFLink'], 'strip' );
     update_post_meta($post->ID, 'PDFLink', $PDFLink);
 }
 
