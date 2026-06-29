8bcac909a0ecc413eccc7c7fbd723873651a6bc2
spenguin <inof@soaringpenguin.com>
2026-06-29 15:14:13 -0700

    Style Home page.

--- includes/shortcodes/products_listing.php
+++ includes/shortcodes/products_listing.php
@@ -5,5 +5,28 @@
 
 function products_listing( $atts = [], $content = null, $tag = '' )
 { 
-    echo '<p>Products listing</p>';
+    $args   = [
+        'post_type'         => 'product',
+        'posts_per_page'    => -1
+    ];
+
+    $query = new WP_Query($args);
+
+    if( $query->have_posts()): ?>
+        <div class="products max-wrapper">
+            <?php
+                while($query->have_posts()): $query->the_post(); //var_dump( get_post_taxonomies( get_the_ID() ));
+                    $terms = get_the_terms( get_the_ID(), 'product_brand');//var_dump($terms);//wp_get_post_terms( get_the_ID(), ['product_brand'], ['fields'=>'name'] );?>
+                    <div class="products__item">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('medium'); ?>
+                            <?php the_title('<p class="products__item--title">', '</p>'); ?>
+                            <p class="products__item--publisher"><?php echo $terms[0]->name; ?></p>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+        </div>
+    <?php endif; wp_reset_postdata();
+
+
 }
